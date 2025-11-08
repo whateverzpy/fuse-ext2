5582d27cfb40a0190b66481c2d8c6ffefcf34a2c
whateverzpy <239275864@qq.com>
2025-11-08 16:41:57 +0800

    完成任务一：简单的文件系统demo

--- fs/demo/src/main.c
+++ fs/demo/src/main.c
@@ -10,31 +10,31 @@
 #include "unistd.h"
 #include "string.h"
 
-#define DEMO_DEFAULT_PERM        0777
-
+#define DEMO_DEFAULT_PERM 0777
 
 /* 超级块 */
 struct demo_super
 {
-    int     driver_fd;  /* 模拟磁盘的fd */
+    int driver_fd; /* 模拟磁盘的fd */
 
-    int     sz_io;      /* 磁盘IO大小，单位B */
-    int     sz_disk;    /* 磁盘容量大小，单位B */
-    int     sz_blks;    /* 逻辑块大小，单位B */
+    int sz_io;   /* 磁盘IO大小，单位B */
+    int sz_disk; /* 磁盘容量大小，单位B */
+    int sz_blks; /* 逻辑块大小，单位B */
 };
 
 /* 目录项 */
 struct demo_dentry
 {
-    char    fname[128];
-}; 
+    char fname[128];
+};
 
 struct demo_super super;
 
 #define DEVICE_NAME "ddriver"
 
 /* 挂载文件系统 */
-static void* demo_mount(struct fuse_conn_info * conn_info){
+static void *demo_mount(struct fuse_conn_info *conn_info)
+{
     // 打开驱动
     char device_path[128] = {0};
     sprintf(device_path, "%s/" DEVICE_NAME, getpwuid(getuid())->pw_dir);
@@ -42,23 +42,23 @@ static void* demo_mount(struct fuse_conn_info * conn_info){
 
     printf("super.driver_fd: %d\n", super.driver_fd);
 
-
     /* 填充super信息 */
-    super.sz_io = /* TODO */;
-    super.sz_disk = /* TODO */;
-    super.sz_blks = /* TODO */; 
+    ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_IO_SZ, &super.sz_io);
+    ddriver_ioctl(super.driver_fd, IOC_REQ_DEVICE_SIZE, &super.sz_disk);
+    super.sz_blks = super.sz_io * 2;
 
     return 0;
 }
 
 /* 卸载文件系统 */
-static void demo_umount(void* p){
+static void demo_umount(void *p)
+{
     // 关闭驱动
     ddriver_close(super.driver_fd);
 }
 
 /* 遍历目录 */
-static int demo_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi)
+static int demo_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
     // 此处任务一同学无需关注demo_readdir的传入参数，也不要使用到上述参数
 
@@ -67,33 +67,40 @@ static int demo_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off
     /* 根据超级块的信息，从第500逻辑块读取一个dentry，ls将只固定显示这个文件名 */
 
     /* TODO: 计算磁盘偏移off，并根据磁盘偏移off调用ddriver_seek移动磁盘头到磁盘偏移off处 */
+    off_t disk_offset = 500 * super.sz_blks;
+    ddriver_seek(super.driver_fd, disk_offset, SEEK_SET);
 
     /* TODO: 调用ddriver_read读出一个磁盘块到内存，512B */
+    char read_buf[super.sz_io];
+    ddriver_read(super.driver_fd, read_buf, super.sz_io);
 
     /* TODO: 使用memcpy拷贝上述512B的前sizeof(demo_dentry)字节构建一个demo_dentry结构 */
+    struct demo_dentry dentry;
+    memcpy(&dentry, read_buf, sizeof(struct demo_dentry));
 
     /* TODO: 填充filename */
+    strcpy(filename, dentry.fname);
 
     // 此处大家先不关注filler，已经帮同学写好，同学填充好filename即可
     return filler(buf, filename, NULL, 0);
 }
 
 /* 显示文件属性 */
-static int demo_getattr(const char* path, struct stat *stbuf)
+static int demo_getattr(const char *path, struct stat *stbuf)
 {
-    if(strcmp(path, "/") == 0)
-        stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFDIR;            // 根目录是目录文件
+    if (strcmp(path, "/") == 0)
+        stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFDIR; // 根目录是目录文件
     else
-        stbuf->st_mode = /* TODO: 显示为普通文件 */;            // 该文件显示为普通文件
+        stbuf->st_mode = DEMO_DEFAULT_PERM | S_IFREG; // 该文件显示为普通文件
     return 0;
 }
 
 /* 根据任务1需求 只用实现前四个钩子函数即可完成ls操作 */
 static struct fuse_operations ops = {
-	.init = demo_mount,						          /* mount文件系统 */		
-	.destroy = demo_umount,							  /* umount文件系统 */
-	.getattr = demo_getattr,							  /* 获取文件属性 */
-	.readdir = demo_readdir,							  /* 填充dentrys */
+    .init = demo_mount,      /* mount文件系统 */
+    .destroy = demo_umount,  /* umount文件系统 */
+    .getattr = demo_getattr, /* 获取文件属性 */
+    .readdir = demo_readdir, /* 填充dentrys */
 };
 
 int main(int argc, char *argv[])
